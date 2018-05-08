1b8f131fe4183c92efbaea745197029217e68eda
BYSora <xinbinyd@163.com>
2018-05-08 10:20:45 +0800

    Update chess.cpp
    最新修改

--- chess.cpp
+++ chess.cpp
@@ -9,8 +9,6 @@
 #include <algorithm>
 #include <queue>
 #define INF 0x3f3f3f3f
-#define MAXN 100005
-#define Mod 1000000007
 using namespace std;
 int changex=8,changey=8;
 struct Node
@@ -101,6 +99,10 @@ int h(int flag,int A[16][16])
                 val+= -1000;
             if(tmp[i][j]!=fflag&&tmp[i][j+1]==flag&&tmp[i][j+2]==flag&&tmp[i][j+3]==flag&&tmp[i][j+4]!=fflag)
                 val+= 1000;
+            /*if(tmp[i][j]!=flag&&tmp[i][j+1]==fflag&&tmp[i][j+2]!=flag&&tmp[i][j+3]==fflag&&tmp[i][j+4]!=flag)
+                val+= -100;
+            if(tmp[i][j]!=fflag&&tmp[i][j+1]==flag&&tmp[i][j+2]!=fflag&&tmp[i][j+3]==flag&&tmp[i][j+4]!=fflag)
+                val+= 100;*/
         }
     for(int i=1; i<=11; ++i) //垂直方向
         for(int j=1; j<=15; ++j)
@@ -109,6 +111,10 @@ int h(int flag,int A[16][16])
                 val+= -1000;
             if(tmp[i][j]!=fflag&&tmp[i+1][j]==flag&&tmp[i+2][j]==flag&&tmp[i+3][j]==flag&&tmp[i+4][j]!=fflag)
                 val+= 1000;
+            /*if(tmp[i][j]!=flag&&tmp[i+1][j]==fflag&&tmp[i+2][j]!=flag&&tmp[i+3][j]==fflag&&tmp[i+4][j]!=flag)
+                val+= -100;
+            if(tmp[i][j]!=fflag&&tmp[i+1][j]==flag&&tmp[i+2][j]!=fflag&&tmp[i+3][j]==flag&&tmp[i+4][j]!=fflag)
+                val+= 100;*/
         }
     for(int i=1; i<=11; ++i) //正斜方向
         for(int j=1; j<=11; ++j)
@@ -117,6 +123,10 @@ int h(int flag,int A[16][16])
                 val+= -1000;
             if(tmp[i][j]!=fflag&&tmp[i+1][j+1]==flag&&tmp[i+2][j+2]==flag&&tmp[i+3][j+3]==flag&&tmp[i+4][j+4]!=fflag)
                 val+= 1000;
+            /*if(tmp[i][j]!=flag&&tmp[i+1][j+1]==fflag&&tmp[i+2][j+2]!=flag&&tmp[i+3][j+3]==fflag&&tmp[i+4][j+4]!=flag)
+                val+= -100;
+            if(tmp[i][j]!=fflag&&tmp[i+1][j+1]==flag&&tmp[i+2][j+2]!=fflag&&tmp[i+3][j+3]==flag&&tmp[i+4][j+4]!=fflag)
+                val+= 100;*/
         }
     for(int i=1; i<=11; ++i) //反斜方向
         for(int j=15; j>=5; --j)
@@ -125,6 +135,10 @@ int h(int flag,int A[16][16])
                 val+= -1000;
             if(tmp[i][j]!=fflag&&tmp[i+1][j-1]==flag&&tmp[i+2][j-2]==flag&&tmp[i+3][j-3]==flag&&tmp[i+4][j-4]!=fflag)
                 val+= 1000;
+            /*if(tmp[i][j]!=flag&&tmp[i+1][j-1]==fflag&&tmp[i+2][j-2]!=flag&&tmp[i+3][j-3]==fflag&&tmp[i+4][j-4]!=flag)
+                val+= -100;
+            if(tmp[i][j]!=fflag&&tmp[i+1][j-1]==flag&&tmp[i+2][j-2]!=fflag&&tmp[i+3][j-3]==flag&&tmp[i+4][j-4]!=fflag)
+                val+= 100;*/
         }
     //出现未堵住的两子，评价次之
     for(int i=1; i<=15; ++i) //水平方向
@@ -159,10 +173,10 @@ int h(int flag,int A[16][16])
             if(tmp[i][j]!=fflag&&tmp[i+1][j-1]==flag&&tmp[i+2][j-2]==flag&&tmp[i+3][j-3]!=fflag)
                 val+= 100;
         }
-    if(val!=0)
+    /*if(val!=0)
     {
         return val;
-    }
+    }*/
     for(int i=1; i<=15; ++i)
         for(int j=1; j<=15; ++j)
             if(tmp[i][j]==8)
@@ -246,7 +260,7 @@ int move0(int A[16][16])
 }
 void solve1(Node &node)
 {
-    int tmp[16][16],f,ans=-INF,anst[16][16];
+    int tmp[16][16],f,ans=-INF,anst[16][16]= {};
     for(int i=1; i<=15; ++i)
         for(int j=1; j<=15; ++j)
             if(node.A[i][j]==8)
@@ -254,7 +268,11 @@ void solve1(Node &node)
                 mcopy(tmp,node.A);
                 tmp[i][j]=1; //自己下
                 if(check(1,tmp))
-                    f=INF;
+                {
+                    f=move1(tmp);
+                    if(f!=-INF)
+                        f=INF;
+                }
                 else
                     f=move1(tmp);
                 //cout<<f<<endl;
@@ -265,8 +283,8 @@ void solve1(Node &node)
                     mcopy(anst,tmp);
                 }
             }
-    for(int i=1;i<=15;++i)
-        for(int j=1;j<=15;++j)
+    for(int i=1; i<=15; ++i)
+        for(int j=1; j<=15; ++j)
             if(node.A[i][j]!=anst[i][j])
             {
                 changex=i;
@@ -279,7 +297,7 @@ void solve1(Node &node)
 }
 void solve0(Node &node)
 {
-    int tmp[16][16],f,ans=-INF,anst[16][16];
+    int tmp[16][16],f,ans=-INF,anst[16][16]= {};
     for(int i=1; i<=15; ++i)
         for(int j=1; j<=15; ++j)
             if(node.A[i][j]==8)
@@ -287,18 +305,22 @@ void solve0(Node &node)
                 mcopy(tmp,node.A);
                 tmp[i][j]=0; //自己下
                 if(check(0,tmp))
-                    f=INF;
+                {
+                    f=move0(tmp);
+                    if(f!=-INF)
+                        f=INF;
+                }
                 else
                     f=move0(tmp);
                 if(f>ans)
                 {
-                    cout<<f<<endl;
+                    //cout<<f<<endl;
                     ans=f;
                     mcopy(anst,tmp);
                 }
             }
-    for(int i=1;i<=15;++i)
-        for(int j=1;j<=15;++j)
+    for(int i=1; i<=15; ++i)
+        for(int j=1; j<=15; ++j)
             if(node.A[i][j]!=anst[i][j])
             {
                 changex=i;
@@ -311,32 +333,51 @@ void solve0(Node &node)
 }
 void show(int A[16][16])
 {
-    for(int i=0; i<=15; ++i)
+    int sum=0;
+    for(int i=1; i<=15; ++i)
+        for(int j=1; j<=15; ++j)
+            sum+=A[i][j];
+    if(sum==0)
+        cout<<"无法继续！"<<endl;
+    else
     {
-        for(int j=0; j<=15; ++j)
+        for(int i=0; i<=15; ++i)
         {
-            if(i==0)
+            for(int j=0; j<=15; ++j)
             {
-                cout<<j<<" ";
-                if(j==0)
+                if(i==0)
+                {
+                    cout<<j<<" ";
+                    if(j==0)
+                        cout<<" ";
+                }
+                else if(j==0)
+                {
+                    cout<<i<<" ";
+                    if(i<10)
+                        cout<<" ";
+                }
+                else if((i==4&&j==4)||(i==4&&j==12)||(i==12&&j==4)||(i==12&&j==12)||(i==changex&&j==changey))
+                {
+                    if(A[i][j]==8)
+                        cout<<".<";
+                    else
+                        cout<<A[i][j]<<"<";
+                }
+                else
+                {
+                    if(A[i][j]==8)
+                        cout<<". ";
+                    else
+                        cout<<A[i][j]<<" ";
+                }
+                if(j>9&&i!=0)
                     cout<<" ";
             }
-            else if(j==0)
-            {
-                cout<<i<<" ";
-                if(i<10)
-                    cout<<" ";
-            }
-            else if((i==4&&j==4)||(i==4&&j==12)||(i==12&&j==4)||(i==12&&j==12)||(i==changex&&j==changey))
-                cout<<A[i][j]<<"<";
-            else
-                cout<<A[i][j]<<" ";
-            if(j>9&&i!=0)
-                cout<<" ";
+            cout<<endl;
         }
         cout<<endl;
     }
-    cout<<endl;
 }
 int main()
 {
@@ -346,7 +387,7 @@ int main()
     for(int i=1; i<=15; ++i)
         for(int j=1; j<=15; ++j)
             node.A[i][j]=8;
-    cout<<"黑棋(1)or白棋(0)?";
+    cout<<"程序执黑棋(1)or白棋(0)?";
     cin>>myturn;
     while(1)
     {
@@ -386,7 +427,6 @@ int main()
             else
             {
                 cout<<"对方的选择是?";
-
                 cin>>i>>j;
                 node.A[i][j]=0;
             }
@@ -396,3 +436,4 @@ int main()
     }
     return 0;
 }
+
